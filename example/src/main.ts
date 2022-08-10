c6cf337bc83f10e0661be5a789766d833a459bdc
yzj <2538340934@qq.com>
2022-08-10 11:41:24 +0800

    更新 example

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,19 +3,17 @@ import App from "./App.vue";
 import "normalize.css";
 import "animate.css";
 
-import {} from "animation-vue";
-import { GlobalAnimateConfig } from "../../dist/common/config";
+import { vueAnimateCss } from "animation-vue";
 
 const app = createApp(App);
 // app.use(bounceDirective);
 // app.use(flashDirective);
 app.use(
-  vueAnimateCss(
-    GlobalAnimateConfig.init({
-      duration: 1000,
-      delay: 1000,
-      repeat: 1
-    })
-  )
+  vueAnimateCss()
+  // GlobalAnimateConfig.init({
+  //   duration: 1000,
+  //   delay: 1000,
+  //   repeat: 1
+  // })
 );
 app.mount("#app");
